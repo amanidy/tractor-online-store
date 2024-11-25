1c2dc634d8aefb5772ea069c5ca630b90503fb8e
Amani <amaniarnold08@gmail.com>
2024-11-25 20:21:52 +0300

    Added prisma

--- src/app/pages/register/page.tsx
+++ src/app/pages/register/page.tsx
@@ -27,7 +27,7 @@ export default function Register() {
 
         try { 
 
-        const response = await fetch("/api/register", {
+        const response = await fetch("/api/register/route.ts", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
