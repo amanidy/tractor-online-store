e95aa8e7b31cec84df958586d96b0d09cd75685a
Amani <amaniarnold08@gmail.com>
2024-12-20 10:03:14 +0300

    updated attachment api to context

--- app/api/tractor/[tractorId]/attachments/[attachmentId]/route.ts
+++ app/api/tractor/[tractorId]/attachments/[attachmentId]/route.ts
@@ -1,41 +1,42 @@
-import { auth } from "@clerk/nextjs/server"
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import { db } from "../../../../../lib/db";
 
 export async function DELETE(
-    req: Request,
-    {params}:{params :{tractorId:string,attachmentId:string}}
+  request: Request,
+  context: {
+    params: {
+      tractorId: string;
+      attachmentId: string;
+    };
+  }
 ) {
-    try {
-        const { userId } = await auth();
-        if (!userId) {
-            return new NextResponse("Unauthorized", { status: 401 });
-        }
-
-        const sellerOwner = await db.tractor.findUnique({
-            where: {
-                id: params.tractorId,
-                sellerId: userId,
-                
-            }
-        });
-        if (!sellerOwner) {
-            return new NextResponse("Unauthorized",{status:401})
-        }
-
-
-        const attachment = await db.attachment.delete({
-            where: {
-                tractorId: params.tractorId,
-                id: params.attachmentId,
-            }
-        });
-
-        return NextResponse.json(attachment);
-
-        
-    } catch (error) {
-        console.log("ATTACHMENT_ID", error)
-        return new NextResponse("Internal server error",{status:500})
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const sellerOwner = await db.tractor.findUnique({
+      where: {
+        id: context.params.tractorId,
+        sellerId: userId,
+      },
+    });
+    if (!sellerOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const attachment = await db.attachment.delete({
+      where: {
+        tractorId: context.params.tractorId,
+        id: context.params.attachmentId,
+      },
+    });
+
+    return NextResponse.json(attachment);
+  } catch (error) {
+    console.log("ATTACHMENT_ID", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
 }
